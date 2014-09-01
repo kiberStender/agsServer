c721e91d858a0464c6fea84830865d1b3cc7ddf4
Kleber Stender <kleber.stender@gmail.com>
2014-09-01 07:05:48 -0300

    Created GameMessages

---
+++ app/ags/messages/GameMessages.scala
@@ -0,0 +1,24 @@
+package ags.messages
+
+import play.api.libs.iteratee.Concurrent.Channel
+import play.api.libs.json.JsValue
+
+/**
+ * Created by sirkleber on 01/09/14.
+ */
+
+trait GameStatus
+
+case class GameConnected() extends GameStatus
+
+case class GameNotConnected() extends GameStatus
+
+case class GameCannotConnect() extends GameStatus
+
+case class GameConnect(ip: String, nome: String, channel: Channel[JsValue])
+
+case class GameDisconnect()
+
+case class SendData(id: Int, data: JsValue)
+
+case class GameMessage(nome: Option[String])
