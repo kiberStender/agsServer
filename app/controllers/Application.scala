b08de700f4a8dd8034ef87404fff199f5b49ae5f
Kleber Stender <kleber.stender@gmail.com>
2014-09-02 20:44:22 -0300

    created index method returning the S.O, the name of the machine and whether it is an application or not

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,26 @@
 package controllers
 
-import play.api._
+import java.net.InetAddress
+
 import play.api.mvc._
+import scala.util.Try
+import scala.language.postfixOps
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.json._
 
-object Application extends Controller {
+object Application extends WebSocketController {
 
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  private def getHost = Try(InetAddress.getLocalHost.getHostName) getOrElse "Unknown"
+
+  def index = Action.async { request =>
+    Future[JsValue]{
+      Json.obj(
+        "application" -> true,
+        "name" -> getHost,
+        "os" -> System.getProperty("os.name")
+      )
+    }.map{ res => Ok(res) as jsonApp }
   }
 
 }
