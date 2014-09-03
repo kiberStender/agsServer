2feeaae5ecda3382834104f0a24f97bce6fcec78
Kleber Stender <kleber.stender@gmail.com>
2014-09-02 21:26:57 -0300

    Created disconnect method

--- app/controllers/ControllerWebSocket.scala
+++ app/controllers/ControllerWebSocket.scala
@@ -4,16 +4,25 @@ package controllers
  * Created by sirkleber on 30/08/14.
  */
 
+import play.api.libs.json.JsValue
 import play.api.mvc.Action
 import akka.pattern.ask
 import ags.actors.Actors
-import ags.messages.PlayerConnect
+import ags.messages.{PlayerDisconnect, PlayerConnect}
+import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.json._
 
 object ControllerWebSocket extends WebSocketController {
 
   def connect = Action.async { req =>
-    (Actors.players ? PlayerConnect(req.remoteAddress)).map{ res =>
+    (Actors.players ? PlayerConnect(req.remoteAddress)).mapTo[JsValue].map{ res =>
       Ok(res) as jsonApp
+    }
+  }
+
+  def disconnect = Action.async {req =>
+    (Actors.players ? PlayerDisconnect(req.remoteAddress)).mapTo[JsValue].map { _ =>
+      Ok(Json.obj("disconnect" -> true)) as jsonApp
     }
   }
 
